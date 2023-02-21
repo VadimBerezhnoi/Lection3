2c2cf2db3a3264858c38f3d43036e6a9d3ba7530
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-21 19:31:15 +0300

    Незначительные изменения

--- Example_003/Example_003.cs
+++ Example_003/Example_003.cs
@@ -10,8 +10,21 @@ void PrintArray(int[, ] matr)
         {
             Console.Write($"{matr[rows, columns]} ");
         }
-        Console.WriteLine();
     }
 }
 
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
 PrintArray(matrix);
