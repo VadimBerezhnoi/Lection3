40ca9a726bff22c2ef8160d15f10d1e27af44ec4
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-21 19:16:29 +0300

    Написана программа, создающая матрицу чисел

---
+++ Example_002/Example_002.cs
@@ -0,0 +1,10 @@
+﻿int[, ] matrix = new int[3, 4];
+
+for (int rows = 0; rows < 3; rows++)
+{
+    for (int columns = 0; columns < 4; columns++)
+    {
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
