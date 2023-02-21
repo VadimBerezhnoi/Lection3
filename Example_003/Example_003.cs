3b650ab078f36534a06a2777dcd6532fcd3c1957
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-21 19:29:16 +0300

    Написана программа с методом, выводящим массив на экран и генерирующая в нем псевдослучайные числа

---
+++ Example_003/Example_003.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[, ] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
